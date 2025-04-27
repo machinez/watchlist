8998aac9e773a1994da2ad0429434b3db56c3841
vanish <zhongziqin33@gmail.com>
2025-04-27 15:55:35 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -10,11 +15,21 @@ def user_page(name):
     return f'User: {escape(name)}'
 
 
-@app.route('/')
-def hello():
-    return "Hello!"
-
-
 @app.route('/test')
 def test_url_for():
     return url_for('user_page', name='peter')
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1998'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
